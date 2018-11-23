10a2afbeb89eb5b4f95080437e89cef9ba1a7fe3
Salomao Rodrigues <salomao88@gmail.com>
2018-11-23 22:08:21 +0000

    Remove some of the boilerplate

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            <Button color="danger">Danger!</Button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import './App.css';
+import MainLayout from './MainLayout';
+
+const App = () => (
+  <MainLayout/>
+);
 
 export default App;
---
+++ src/MainLayout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const MainLayout = () => (
+  <main id="main-layout">
+    <nav>Navigation bar</nav>
+    <div>Poc here</div>
+    <footer>Copyright © 2018 Wishing Portal Ltd</footer>
+  </main>
+);
+
+export default MainLayout;
