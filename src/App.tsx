bf8fac6acbcae1fda26789374925637a15a987d2
Salomao Rodrigues <salomao88@gmail.com>
2018-12-10 17:06:09 +0000

    WIP - Trying dummy login form and test server response

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,114 @@
 import React, { SFC } from 'react';
+import { graphql, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { get } from 'lodash-es';
 
 import MainLayout from './MainLayout';
 
+const SIGN_IN_MUTATION = gql`
+  mutation signIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      id
+      username
+      email
+      createdAt
+    }
+  }
+`;
+
+interface AuthFormFields {
+  email: string;
+  password: string;
+  user: object;
+  errors: Array<object>;
+  [key: string]: any;
+}
+
+class AuthPiece extends React.Component<{}, AuthFormFields> {
+  state = {
+    email: '',
+    password: '',
+    errors: [],
+    user: {}
+  };
+
+  readonly onChangeHandler = ({
+    currentTarget: { name, value }
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ [name]: value });
+  };
+
+  getErrorMessages = (errors: { graphQLErrors: any[] }): string[] => {
+    return errors.graphQLErrors.reduce((messages: string[], error: object) => {
+      const message = get(error, 'extensions.exception.details[0].message');
+
+      if (message) {
+        messages.push(message);
+      }
+
+      return messages;
+    }, []);
+  };
+
+  render(): React.ReactNode {
+    const { email, password } = this.state;
+    return (
+      <Mutation mutation={SIGN_IN_MUTATION}>
+        {(signIn, { error }) => (
+          <div>
+            <form
+              onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                signIn({ variables: { email, password } });
+                this.setState({ email: '', password: '' });
+              }}
+            >
+              <div>
+                <label>
+                  Email: <input name="email" onChange={this.onChangeHandler} />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Password:{' '}
+                  <input name="password" onChange={this.onChangeHandler} />
+                </label>
+              </div>
+              <button type="submit">Login</button>
+              <button type="button" onClick={console.log}>
+                Logout
+              </button>
+              {/* { loading && <div>Loading...</div>} */}
+              {error && (
+                <div style={{ color: 'red' }}>
+                  <div />{' '}
+                  {this.getErrorMessages(error).map((message, index) => (
+                    <div key={ index }>{ message }</div>
+                  ))}
+                </div>
+              )}
+            </form>
+          </div>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+const AuthPieceWithClient = graphql(SIGN_IN_MUTATION)(AuthPiece);
+
+// query QueryName {
+//   me {
+//     id
+//     email
+//     username
+//   }
+// }
+
 const App: SFC<{}> = () => (
-  <MainLayout/>
+  <MainLayout>
+    <AuthPieceWithClient />
+  </MainLayout>
 );
 
 export default App;
--- src/MainLayout/index.tsx
+++ src/MainLayout/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import './index.scss';
 
-const MainLayout = () => (
+interface MainLayoutProps {
+  children?: ReactNode[] | ReactNode
+};
+
+const MainLayout: React.SFC<MainLayoutProps> = ({ children }) => (
   <main id="main-layout">
     <nav>Navigation bar</nav>
+    { children }
     <div>Poc here</div>
     <footer>Copyright © 2018 Wishing Portal Ltd</footer>
   </main>
